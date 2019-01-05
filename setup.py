3de0843c49481ae6a349e58f089dd69bfcf855e7
Simo Tumelius <simo.tumelius@gmail.com>
2019-01-05 09:35:55 +0200

    Add seppuku.message module with write_message and read_message functions
    * Add setup.py script
    * Add pytest and travis configs

---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from seppuku import __version__
+
+setup(
+    name='seppuku',
+    version=__version__,
+    description='REST API for writing and reading self-destructing messages with '
+                'optional auto-expiry timers using a redis backend.',
+    author='Simo Tumelius',
+    author_email='simo.tumelius@gmail.com',
+    url='https://github.com/smomni/self-destructing-messages-api',
+    python_requires='>=3.6',
+    license='MIT',
+    classifiers=[
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    packages=find_packages(),
+    install_requires=[],
+    extras_require={
+        'dev': [],
+        'test': ['pytest', 'pytest-cov'],
+        'docs': []
+    },
+    entry_points={
+        'console_scripts': [],
+    },
+)
---
+++ test/test_message.py
@@ -0,0 +1,49 @@
+import pytest
+import time
+from redis import Redis
+from seppuku.message import write_message, read_message
+
+
+@pytest.fixture
+def redis():
+    return Redis(host='localhost', port=6379)
+
+
+def test_write_message_with_message_arg_returns_random_key(redis):
+    key = write_message(value='pytest', backend=redis)
+    assert isinstance(key, str)
+
+
+def test_write_message_with_message_arg_writes_message_to_redis(redis):
+    key = write_message(value='pytest', backend=redis)
+    assert 'pytest' == redis.get(key).decode()
+
+
+def test_write_message_with_message_and_expire_args_deletes_key_after_specified_time_in_seconds(redis):
+    time_in_seconds = 1
+    key = write_message(value='pytest', expire=time_in_seconds, backend=redis)
+    time.sleep(time_in_seconds + 1)
+    with pytest.raises(ValueError):
+        read_message(key=key, backend=redis)
+
+
+def test_read_message_with_key_arg_returns_value(redis):
+    key = write_message(value='pytest', backend=redis)
+    value = read_message(key=key, backend=redis)
+    assert 'pytest' == value
+
+
+def test_read_message_with_key_arg_deletes_the_key_from_redis(redis):
+    key = write_message(value='pytest', backend=redis)
+    # Message can be read only once
+    read_message(key=key, backend=redis)
+    with pytest.raises(ValueError):
+        read_message(key=key, backend=redis)
+
+
+def test_read_message_invalid_key_raises_value_error(redis):
+    with pytest.raises(ValueError):
+        read_message(key='non-existing key!', backend=redis)
+
+
+
